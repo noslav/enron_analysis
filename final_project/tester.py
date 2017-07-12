7a473601a26a3c379aacf7b0c7d047a4d82cb3cf
Noslav <pranay.valson@gmail.com>
2017-07-12 08:01:36 +0200

    reverted to DT and also made changes to py notebook report

--- final_project/tester.py
+++ final_project/tester.py
@@ -122,7 +122,7 @@ def featureNoSelect(X, y):
 # The "accuracy" scoring is proportional to the number of correct
 # classifications
     rfecv = RFECV(estimator=svc, step=1, cv=StratifiedKFold(6),
-              scoring='precision')
+              scoring='accuracy')
     rfecv.fit(X, y)
 
     print("Optimal number of features : %d" % rfecv.n_features_)
@@ -133,3 +133,4 @@ def featureNoSelect(X, y):
     plt.ylabel("Cross validation score (nb of correct classifications)")
     plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
     plt.show()      
+
